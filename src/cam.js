73be8725b40c26987ed2527a10b48f0c81c6621c
Harshal patil <harshalmukundapatil@gmail.com>
2024-02-14 11:19:06 +0530

    init commit

---
+++ src/cam.js
@@ -0,0 +1,11 @@
+import Webcam from "react-webcam";
+
+const CustomWebcam = () => {
+  return (
+    <div className="container">heheheheh
+      {/* <Webcam height={600} width={300} /> */}
+    </div>
+  );
+};
+
+export default CustomWebcam;

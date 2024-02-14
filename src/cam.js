b3e081a4eb918a82cdf795a463246c42deb57580
Harshal patil <harshalmukundapatil@gmail.com>
2024-02-14 11:24:49 +0530

    cam init

--- src/cam.js
+++ src/cam.js
@@ -3,7 +3,7 @@ import Webcam from "react-webcam";
 const CustomWebcam = () => {
   return (
     <div className="container">heheheheh
-      {/* <Webcam height={600} width={300} /> */}
+      <Webcam height={600} width={300} />
     </div>
   );
 };
